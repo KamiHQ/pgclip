c18290962a60cde0e2dcc4e16443aa6c121d112b
Jordan Thoms <jordan@thoms.net.nz>
2014-11-07 14:41:13 +1300

    Use database pool

--- unicorn.rb
+++ unicorn.rb
@@ -16,7 +16,11 @@ after_fork do |server, worker|
   Signal.trap 'TERM' do
     puts 'Unicorn worker intercepting TERM and doing nothing. Wait for master to send QUIT'
   end
-
-  defined?(ActiveRecord::Base) and
-    ActiveRecord::Base.establish_connection
+  # other settings
+  if defined?(ActiveRecord::Base)
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['pool']            =   ENV['DB_POOL'] || 2
+    ActiveRecord::Base.establish_connection(config)
+  end
 end
